bfbfbe43ae05e01761148c842612dc64be260f5d
Janis <janis@vaio>
2019-03-23 11:40:59 +0100

    Added files for login and projects Tested some vue features

--- Frontend/group5-client/src/router.js
+++ Frontend/group5-client/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Projects from './views/Projects.vue';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
@@ -8,6 +10,16 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'projects',
+      component: Projects,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home,
     },
