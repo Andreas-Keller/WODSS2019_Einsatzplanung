5ece9053379bac34e84e3e99b139c3b46952949b
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-03-25 17:34:07 +0100

    Update requestToken.js

--- Backend/handlers/requestToken.js
+++ Backend/handlers/requestToken.js
@@ -10,6 +10,6 @@
  */
 exports.handler = function requestToken(req, res, next) {
     token = {token: 'eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJGSE5XIHdvZHNzIiwic3ViIjoiTG9naW4gdG9rZW4iLCJlbXBsb3llZSI6eyJpZCI6MSwiZmlyc3ROYW1lIjoiU2ltb24iLCJsYXN0TmFtZSI6IldhZWNodGVyIiwiZW1haWxBZGRyZXNzIjoic2ltb24ud2FlY2h0ZXJAc3R1ZGVudHMuZmhudy5jaCIsInJvbGUiOiJBRE1JTklTVFJBVE9SIiwiYWN0aXZlIjp0cnVlfSwiaWF0IjoxNTE2MjM5MDIyfQ.3eFaVQ_QOJSFq8CnIudMGNnvzCQFRblTR9K5yxXmfClczT3UN6ZrFZ5eKUc0L1H-67ROPv5hz_odMq867P9ofA'}
-    res.send(token)
+    res.status(201).send(token)
     next()
 }
