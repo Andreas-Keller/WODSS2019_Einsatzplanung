9d8638b5c305a97051093cffe6b02c74809d5930
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-03-25 17:20:12 +0100

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -17,6 +17,14 @@ const gstore = new Gstore();
 const datastore = new Datastore();
 
 gstore.connect(datastore);
+
+
+var bodyParser = require('body-parser');
+var cors = require('cors');
+app.options('*', cors());  // enable pre-flight
+app.use(bodyParser.json());
+
+
 const PORT = process.env.PORT || 8080;
 app.listen(process.env.PORT || 8080, () => {
     console.log(`App listening on port ${PORT}`);
