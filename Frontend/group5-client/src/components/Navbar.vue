567ec8749f187e30351c73341d2aaab892479652
Fallyx <janis.angst@gmail.com>
2019-03-27 12:43:17 +0100

    Fixed linting errors Added Logout logic

--- Frontend/group5-client/src/components/Navbar.vue
+++ Frontend/group5-client/src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <b-navbar-brand tag="h1" class="mb-0">Job Scheduler</b-navbar-brand>
     <b-navbar-nav class="ml-auto">
       <b-nav-text>{{employeeName}}</b-nav-text>
-      <b-nav-item to="/login">Logout</b-nav-item>
+      <b-nav-item @click="logout">Logout</b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
@@ -13,6 +13,12 @@ export default {
   name: 'Navbar',
   props: {
     employeeName: String,
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+      this.$router.push({ name: 'login' });
+    },
   },
 };
 </script>
