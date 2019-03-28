7ea9abac52228aa641705f1938f260d2cbd0b1a4
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-03-28 16:02:04 +0100

    added get employee_s

--- Backend/handlers/getEmployees.js
+++ Backend/handlers/getEmployees.js
@@ -2,12 +2,14 @@
  * Get all employees
  *
  * GET: /api/employee
- * 
+ *
  * query:
  *   role {string} Filter the employees by role).
- *   
+ *
  */
-exports.handler = function getEmployees(req, res, next) {
-  res.send('getEmployees');
-  next()
+exports.handler = async function getEmployees(req, res, next) {
+    // res.send('getEmployees');
+    const employeeFirebase = require('../firebase/employee.crud.js');
+    res.status(200).send(await employeeFirebase.getEmployees());
+    next()
 };
