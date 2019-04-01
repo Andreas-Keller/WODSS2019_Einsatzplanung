e9e9137fbb18650c2defb7283c0c12c6990ba57a
Andreas-Keller <andreaskeller2010@gmail.com>
2019-04-01 12:42:41 +0200

    add second firebase db for testing

--- Backend/firebase/firebase.admin.js
+++ Backend/firebase/firebase.admin.js
@@ -1,9 +1,20 @@
 const admin = require("firebase-admin");
 
+/*
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     databaseURL: "https://wodss2019.firebaseio.com"
 });
+*/
+
+
+var serviceAccount = require("/Users/andreas/Desktop/wodss2019-xyz-firebase-adminsdk-9ukeg-290c673a0a.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://wodss2019-xyz.firebaseio.com",
+});
+
 
 const db = admin.firestore();
 
