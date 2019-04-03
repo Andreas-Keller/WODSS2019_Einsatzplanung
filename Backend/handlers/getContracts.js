ac53c26ee8744530fe09f2a2cb5d5ae9aeda57cd
Andreas-Keller <andreaskeller2010@gmail.com>
2019-04-03 14:17:06 +0200

    first implementation of vue component "contracts"

--- Backend/handlers/getContracts.js
+++ Backend/handlers/getContracts.js
@@ -2,13 +2,15 @@
  * Get all contracts
  *
  * GET: /api/contract
- * 
+ *
  * query:
  *   fromDate {date} Start date (YYYY-MM-DD) to create a time range with a lower boundary (Contracts with a start date be...
  *   toDate {date} End date (YYYY-MM-DD) to create a time range with an upper boundary (Contracts with a start date bef...
- *   
+ *
  */
-exports.handler = function getContracts(req, res, next) {
-  res.send('getContracts')
+exports.handler = async function getContracts(req, res, next) {
+  //res.send('getContracts')
+  const contractFirebase = require('../firebase/contract.crud.js');
+  res.status(200).send(await contractFirebase.getContracts());
   next()
 }
