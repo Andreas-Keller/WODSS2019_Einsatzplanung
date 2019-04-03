f295255447f63020b21c7481a27d46a6f6ba49ae
Andreas-Keller <andreaskeller2010@gmail.com>
2019-04-03 15:04:45 +0200

    first implementation of component "projects"

--- Backend/handlers/getProjects.js
+++ Backend/handlers/getProjects.js
@@ -2,14 +2,16 @@
  * Get all projects
  *
  * GET: /api/project
- * 
+ *
  * query:
  *   projectManagerId {int64} Filter the projects by a project manager ID.
  *   fromDate {date} Start date (YYYY-MM-DD) to create a time range with a lower boundary (Projects with a start date bef...
  *   toDate {date} End date (YYYY-MM-DD) to create a time range with an upper boundary (Projects with a start date befo...
- *   
+ *
  */
-exports.handler = function getProjects(req, res, next) {
-  res.send('getProjects')
+exports.handler = async function getProjects(req, res, next) {
+  const projectFirebase = require('../firebase/project.crud.js');
+  res.status(200).send(await projectFirebase.getProjects());
+  //res.send('getProjects')
   next()
 }
