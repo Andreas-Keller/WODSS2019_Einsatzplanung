9707525a9113c28a07a7399b5b0bfb0669c28f0d
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-04-08 14:38:30 +0200

    added delete for all

--- Backend/handlers/deleteAllocation.js
+++ Backend/handlers/deleteAllocation.js
@@ -2,12 +2,14 @@
  * Delete a specific allocation
  *
  * DELETE: /api/allocation/{id}
- * 
+ *
  * path:
  *   id {int64} ID of the allocation to be deleted.
- *   
+ *
  */
-exports.handler = function deleteAllocation(req, res, next) {
-  res.send('deleteAllocation')
-  next()
+exports.handler = async function deleteAllocation(req, res, next) {
+    const fb = require('../firebase/allocation.crud');
+    let response = await fb.deleteAllocation(req.params.id);
+    res.status(response.httpStatus).send(response.payload);
+    next();
 }
--- Backend/handlers/deleteProject.js
+++ Backend/handlers/deleteProject.js
@@ -2,12 +2,14 @@
  * Delete a specific project including all associated allocations (Note: Cascading delete)
  *
  * DELETE: /api/project/{id}
- * 
+ *
  * path:
  *   id {int64} ID of the project to be deleted.
- *   
+ *
  */
-exports.handler = function deleteProject(req, res, next) {
-  res.send('deleteProject')
-  next()
+exports.handler = async function deleteProject(req, res, next) {
+    const fb = require('../firebase/project.crud');
+    let response = await fb.deleteProject(req.params.id);
+    res.status(response.httpStatus).send(response.payload);
+    next();
 }
