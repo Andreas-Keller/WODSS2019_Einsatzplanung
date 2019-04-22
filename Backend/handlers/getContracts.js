4e6fa4db173a800f2e69b0a3287ab7a28897c531
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-04-22 19:15:30 +0200

    finished getContracts

--- Backend/handlers/getContracts.js
+++ Backend/handlers/getContracts.js
@@ -12,6 +12,44 @@ exports.handler = async function getContracts(req, res, next) {
     //res.send('getContracts')
     const contractFirebase = require('../firebase/contract.crud.js');
     let response = await contractFirebase.getContracts();
+
+    const util = require("../util/Util");
+    const user = util.decodeToken(req);
+    if (response.httpStatus === 200) {
+        if (user.role === "DEVELOPER") {
+            let data = [];
+            //get all contracts which match with employee
+            for (let i = 0; i < response.payload.length; i++) {
+                if (response.payload[i].employeeId == user.id) {
+                    data.push(response.payload[i]);
+                }
+            }
+            response.payload = data;
+        }
+        //filters
+        if (req.query.fromDate) {
+            let data = [];
+            for (let i = 0; i < response.payload.length; i++) {
+                if (response.payload[i].startDate >= req.query.fromDate) {
+                    data.push(response.payload[i]);
+                }
+            }
+            response.payload = data;
+        }
+        if (req.query.toDate) {
+            let data = [];
+            for (let i = 0; i < response.payload.length; i++) {
+                if (response.payload[i].endDate <= req.query.toDate) {
+                    data.push(response.payload[i]);
+                }
+            }
+            response.payload = data;
+        }
+        res.status(response.httpStatus).send(response.payload);
+    } else {
+        res.status(response.httpStatus).send();
+    }
+
     res.status(response.httpStatus).send(response.payload);
     next();
-}
+};
