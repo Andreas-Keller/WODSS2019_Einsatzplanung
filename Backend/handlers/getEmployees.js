df94fa1e6be9a1cfd03d4de0d6d6d72c8b6868a9
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-04-22 19:25:13 +0200

    finished getEmployees, filter and returns only dev when dev

--- Backend/handlers/getEmployees.js
+++ Backend/handlers/getEmployees.js
@@ -8,29 +8,35 @@
  *
  */
 exports.handler = async function getEmployees(req, res, next) {
-    // res.send('getEmployees');
     const employeeFirebase = require('../firebase/employee.crud.js');
     let response = await employeeFirebase.getEmployees();
 
-    let data = response.payload;
-    let array = [];
-
-    if(req.query.role != undefined) {
-        for(let i = 0; i < response.payload.length; i++) {
-            if(response.payload[i].role === req.query.role && response.payload[i].emailAddress !== null) {
-                array.push(response.payload[i]);
-            }
+    const util = require("../util/Util");
+    const user = util.decodeToken(req);
+    if (response.httpStatus === 200) {
+        if (user.role === "DEVELOPER") {
+            response.payload = [user];//return only dev\
         }
-        data = array;
+        //filters
+        let data = [];
+        if (req.query.role != undefined) {
+            for (let i = 0; i < response.payload.length; i++) {
+                if (response.payload[i].role === req.query.role && response.payload[i].emailAddress !== null) {
+                    data.push(response.payload[i]);
+                }
+            }
+            response.payload = data;
+        } else {
+            for (let i = 0; i < response.payload.length; i++) {
+                if (response.payload[i].emailAddress !== null) {
+                    data.push(response.payload[i]);
+                }
+            }
+            response.payload = data;
+        }
+        res.status(response.httpStatus).send(response.payload);
     } else {
-        for(let i = 0; i < response.payload.length; i++) {
-            if(response.payload[i].emailAddress !== null) {
-                array.push(response.payload[i]);
-            }
-        }
-        data = array;
+        res.status(response.httpStatus).send();
     }
-
-    res.status(response.httpStatus).send(data);
-    next()
+    next();
 };
