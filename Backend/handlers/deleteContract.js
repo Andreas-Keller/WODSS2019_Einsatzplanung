a60d74c952895262dec5adbac1cc1cde098133a8
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-04-24 12:35:00 +0200

    added checks at delete contract

--- Backend/handlers/deleteContract.js
+++ Backend/handlers/deleteContract.js
@@ -2,14 +2,25 @@
  * Delete a specific contract (Note: The contract can only be deleted as long as he is not used for an allocation)
  *
  * DELETE: /api/contract/{id}
- * 
+ *
  * path:
  *   id {int64} ID of the contract to be deleted.
- *   
+ *
  */
 exports.handler = async function deleteContract(req, res, next) {
     const fb = require('../firebase/contract.crud');
-    let response = await fb.deleteContract(req.params.id);
-    res.status(response.httpStatus).send(response.payload);
+    const afb = require('../firebase/allocation.crud');
+    let contract = await fb.getContract(req.params.id);
+    if (contract.httpStatus != 200) {
+        res.status(contract.httpStatus).send(contract.payload);
+    } else {
+        let allocations = await afb.findAllBy("contractId", contract.payload.id)
+        if (allocations.payload.length == 0) {
+            let response = await fb.deleteContract(req.params.id);
+            res.status(204).send(response.payload);
+        } else {
+            res.status(412).send("Precondition for the contract failed");
+        }
+    }
     next();
 }
