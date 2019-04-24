58850625fc33201e3b2661d3a4c48d98bd9c17d3
Matthias Ernst <matthias.s.ernst@gmail.com>
2019-04-24 10:12:29 +0200

    Added date conflict handling in createContract

--- Backend/handlers/createContract.js
+++ Backend/handlers/createContract.js
@@ -33,10 +33,26 @@ exports.handler = async function createContract(req, res, next) {
     } else if (employee.httpStatus === 404) {
         res.status(employee.httpStatus).send("Employee not found")
 
-    } else {
-        const contractFirebase = require('../firebase/contract.crud.js');
-        let response = await contractFirebase.createContract(contract)
-        res.status(response.httpStatus).send(response.payload);
+    } else {//check if contract starts/ends in another contract or if the contract is active while another is
+        let employeeContracts = await require('../firebase/contract.crud').findAllBy('employeeId', employee.payload.id);
+        let canCreate = true;
+        for (const c of employeeContracts.payload) {
+            if ((contract.startDate >= c.startDate && contract.startDate <= c.endDate) || (contract.endDate >= c.startDate && contract.endDate <= c.endDate)) {
+                canCreate = false;
+            }
+            if ((c.startDate >= contract.startDate && c.endDate <= contract.startDate) || (c.startDate >= contract.endDate && c.endDate <= contract.endDate)) {
+                canCreate = false;
+            }
+            console.log(contract.startDate >= c.startDate && contract.startDate <= c.endDate);
+            console.log(contract.endDate >= c.startDate && contract.endDate <= c.endDate);
+        }
+        if (canCreate) {
+            const contractFirebase = require('../firebase/contract.crud.js');
+            let response = await contractFirebase.createContract(contract)
+            res.status(response.httpStatus).send(response.payload);
+        } else {
+            res.status(412).send("Precondition for the contract failed");
+        }
     }
     next()
 };
